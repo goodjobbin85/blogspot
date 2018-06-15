ca32146a59fc784d1c335828e8597f6843a606d7
Thomas Dubiel <timidturtle85@gmail.com>
2018-06-15 10:36:07 -0500

    add password functionality to user and add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+	validates :name, presence: true
+	validates :name, length: { minimum: 4, maximum: 25 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 100 }, 
+	          uniqueness: { case_sensitive: false },
+	          format: { with: VALID_EMAIL_REGEX }
+
+
+	has_secure_password
 end

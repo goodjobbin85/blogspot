b1532c9614f601c81034aa49f3c0d8975923f93e
Thomas Dubiel <timidturtle85@gmail.com>
2018-08-19 11:16:51 -0500

    update updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+   before_action :number_of_requests_in_session
 
    include SessionsHelper
+
+   def number_of_requests_in_session 
+   	session[:requests] ||= 0 
+   	session[:requests] += 1
+   end
+
+   
+
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,4 +12,8 @@ module SessionsHelper
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def show_session_requests 
+   	"number of requests is #{session[:requests]}"
+  end
+
 end

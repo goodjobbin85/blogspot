10e5ad003f355d54e90ece5665c3d463f5d3e3d4
Thomas Dubiel <timidturtle85@gmail.com>
2018-09-06 09:44:08 -0500

    test

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+
 
   validates :title, presence: true
   validates :description, presence: true, 
   			length: { minimum: 5, maximum: 150 }
 end
+ ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180617021816) do
+ActiveRecord::Schema.define(version: 20180905215025) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_article_categories_on_article_id"
+    t.index ["category_id"], name: "index_article_categories_on_category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.integer "user_id"
@@ -19,6 +28,12 @@ ActiveRecord::Schema.define(version: 20180617021816) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
